d0baa6633ffddae2850c5f6ace68167e47aa0318
AnisIslam <manisul1997bd@gmail.com>
2023-01-10 22:03:02 +0600

    input

--- python-first-program.py
+++ python-first-program.py
@@ -2,8 +2,11 @@ print("ok")
 print("Anis")
 
 #comment
-if 5 > 3:
-  print("Five is greater than two!")
-x = 5
-y = "Hello, World!"
-print(y)
+# if 5 > 3:
+#   print("Five is greater than two!")
+# x = 5
+# y = "Hello, World!"
+# print(y)
+print("Give a number")
+number = input()
+print("the number is", number)
