85ab15f457fa960833be084e229ebc0e881788e6
AnisIslam <manisul1997bd@gmail.com>
2023-01-23 19:41:02 +0600

    object created multiple

--- class.py
+++ class.py
@@ -42,16 +42,56 @@
 # my_car.start()
 # ----------------------
 
-#corrected code with self
+# #corrected code with self
+# class Car:
+#     name = ""
+#     color = ""
+
+#     def start(self):
+#         print("Starting the engine")
+
+# # creating a Car object
+# my_car = Car()
+# my_car.name = "Allion"
+# print(my_car.name)
+# my_car.start()
+
+# -------------------
+
+# class Car:
+#     # name = ""
+#     # color = ""
+
+#     def __init__(self,name,color):
+#         self.name=name
+#         self.color = color
+    
+#     def start(self):
+#         print("Starting the engine")
+
+# my_car = Car("Corolla", "white")
+# print(my_car.name)
+# print(my_car.color)
+# my_car.start()
+
+# -----------------------------------------
+
+#creating multiple objects
+
 class Car:
-    name = ""
-    color = ""
+    def __init__(self, n, c): #this is a constructor
+        self.name = n
+        self.color = c
 
     def start(self):
+        print("name: ", self.name)
+        print("color: ", self.color)
         print("Starting the engine")
+        print("----------------------------")
 
-# creating a Car object
-my_car = Car()
-my_car.name = "Allion"
-print(my_car.name)
-my_car.start()
+my_car1 = Car("Corolla", "White")
+my_car1.start()
+my_car2 = Car("Premio", "Black")
+my_car2.start()
+my_car3 = Car("Allion", "Blue")
+my_car3.start()
