1da1336b5f084b052ea8a13508dfbe05c6a963c9
AnisIslam <manisul1997bd@gmail.com>
2023-01-23 18:38:46 +0600

    import test

---
+++ math_1.py
@@ -0,0 +1,9 @@
+import math
+from datetime import datetime
+
+today = datetime.today()
+print(today)
+
+import webbrowser
+url = "https://discord.com/channels/893465376464207892/893465423432019998"
+webbrowser.open(url)
