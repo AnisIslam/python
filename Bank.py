d7a4af86c9d531de983d2b90657335d5c4ea3ba4
AnisIslam <manisul1997bd@gmail.com>
2023-02-05 22:10:27 +0600

    bank project

---
+++ Bank.py
@@ -0,0 +1,21 @@
+class Bank:
+    def __init__(self, balance):
+        self.balance = balance
+        self.min_withdraw = 100
+        self.max_withdraw = 10000
+
+    def get_balance(self):
+        return self.balance
+    def withdraw (self , amount):
+        if amount < self.min_withdraw:
+            return "no money for you"
+        elif amount > self.max_withdraw:
+            return f"crosses max limit : {self.max_withdraw}"
+        elif amount > self.balance:
+            return "No money sorry"
+        else:
+            self.balance = self.balance -amount
+            return f"Here is yuor money : {amount}"
+my_bank = Bank(8000)
+reply = my_bank.withdraw(5)
+print(reply)
