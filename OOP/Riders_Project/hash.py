601ea4e254397baf98bc5239885d9a6105df4504
AnisIslam <manisul1997bd@gmail.com>
2023-02-12 19:55:29 +0600

    pass hased ok

--- OOP/Riders_Project/hash.py
+++ OOP/Riders_Project/hash.py
@@ -10,7 +10,7 @@ pwd_encode = pwd.encode()
 pwd_hash = hashlib.md5(pwd_encode).hexdigest()
 
 your_email = 'abcde@gmail.com'
-your_password = '2329e22c9a4de221abeabaf22b72c7fc'
+your_password = 'ChairOnTableWith3Legs'
 
 hashed_your_password = hashlib.md5(your_password.encode()).hexdigest()
 if email == your_email and pwd_hash == hashed_your_password:
