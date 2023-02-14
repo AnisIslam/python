ecec2766339fac161add7f2649665a072bfcab00
AnisIslam <manisul1997bd@gmail.com>
2023-02-14 13:33:41 +0600

    riding match found

--- OOP/Riders_Project/user.py
+++ OOP/Riders_Project/user.py
@@ -2,7 +2,7 @@ import hashlib
 from random import random, randint
 from brta import BRTA
 from vehicle import Bike, Car, Cng
-from ride_manager import uber
+from rideManager import uber
 license_authority = BRTA()
 
 
@@ -107,25 +107,25 @@ result = license_authority.validate_license(driverShaheb1.email, driverShaheb1.l
 print(result)
 driverShaheb1.take_driving_test
 
-# rider1 = Rider('rider1', 'rider1@gmail.com', 'rider1', randint(0, 30), 5000)
-# rider2 = Rider('rider2', 'rider2@gmail.com', 'rider2', randint(0, 30), 5000)
-# rider3 = Rider('rider3', 'rider3@gmail.com', 'rider3', randint(0, 30), 5000)
+rider1 = Rider('rider1', 'rider1@gmail.com', 'rider1', randint(0, 30), 5000)
+rider2 = Rider('rider2', 'rider2@gmail.com', 'rider2', randint(0, 30), 5000)
+rider3 = Rider('rider3', 'rider3@gmail.com', 'rider3', randint(0, 30), 5000)
 
-# driver1 = Driver('driver1', 'driver1@gmail.com', 'driver1', randint(0, 30), 5645)
-# driver1.take_driving_test()
-# driver1.register_a_vehicle('car', 1245, 10)
+driver1 = Driver('driver1', 'driver1@gmail.com', 'driver1', randint(0, 30), 5645)
+driver1.take_driving_test()
+driver1.register_a_vehicle('car', 1245, 10)
 
-# driver2 = Driver('driver2', 'driver2@gmail.com', 'driver2', randint(0, 30), 5645)
-# driver2.take_driving_test()
-# driver2.register_a_vehicle('car', 1245, 10)
+driver2 = Driver('driver2', 'driver2@gmail.com', 'driver2', randint(0, 30), 5645)
+driver2.take_driving_test()
+driver2.register_a_vehicle('car', 1245, 10)
 
-# driver3 = Driver('driver3', 'driver3@gmail.com', 'driver3', randint(0, 30), 5645)
-# driver3.take_driving_test()
-# driver3.register_a_vehicle('car', 2145, 10)
+driver3 = Driver('driver3', 'driver3@gmail.com', 'driver3', randint(0, 30), 5645)
+driver3.take_driving_test()
+driver3.register_a_vehicle('car', 2145, 10)
 
-# driver4 = Driver('driver4', 'driver4@gmail.com', 'driver4', randint(0, 30), 5645)
-# driver4.take_driving_test()
-# driver4.register_a_vehicle('car', 3245, 10)
+driver4 = Driver('driver4', 'driver4@gmail.com', 'driver4', randint(0, 30), 5645)
+driver4.take_driving_test()
+driver4.register_a_vehicle('car', 3245, 10)
 
-# print(uber.get_available_cars())
-# uber.find_a_vehicle(rider1, 'car', 90)
+print(uber.get_available_cars())
+uber.find_a_vehicle(rider1, 'car', 90)
